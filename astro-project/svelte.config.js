0ad9c08577e05da601f08348a6dcbbe9c68b841d
Convolutio <79534557+Convolutio@users.noreply.github.com>
2025-08-20 21:43:06 +0200

    feat: working + updated setup with tailwind

--- astro-project/svelte.config.js
+++ astro-project/svelte.config.js
@@ -1,5 +1,19 @@
-import { vitePreprocess } from '@astrojs/svelte';
+import { sveltePreprocess } from 'svelte-preprocess';
+import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
 
+/** @type {import('@sveltejs/kit').Config} */
 export default {
-	preprocess: vitePreprocess(),
-}
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: [sveltePreprocess({
+    scss: {
+      prependData: `
+@reference '${path.resolve('./src/styles/app.scss')}';
+`
+    },
+    postcss: {
+      plugins: [tailwindcss]
+    },
+  })],
+};

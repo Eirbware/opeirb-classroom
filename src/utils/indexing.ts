177e7f900a32a4d0794982c74c07fb42ab23d12d
Convolutio <79534557+Convolutio@users.noreply.github.com>
2025-12-28 15:38:36 +0100

    feat: post indexing utils

---
+++ src/utils/indexing.ts
@@ -0,0 +1,84 @@
+/** Utils function to index posts
+ */
+import type { PaginateFunction } from "astro";
+import { getCollection, type CollectionEntry, type ReferenceDataEntry } from "astro:content";
+import { filterPerLanguage } from "./filter_language";
+
+const DEFAULT_POSTS_PER_PAGE = 18
+
+type Post = CollectionEntry<"courses" | "tips">
+type PostFilter = (post: Post) => boolean;
+type PostSortCompareFn = (postA: Post, postB: Post) => number
+
+function composeCompareFns<T extends PostSortCompareFn | null>(
+  newPrioritizedCompareFn: PostSortCompareFn | undefined,
+  baseCompareFn: T
+): T {
+  if (!newPrioritizedCompareFn) return baseCompareFn;
+  if (baseCompareFn === null) return baseCompareFn;
+  return ((postA, postB) => {
+    const firstCompareResult = newPrioritizedCompareFn(postA, postB);
+    return (firstCompareResult !== 0) ? firstCompareResult : baseCompareFn(postA, postB);
+  }) as typeof baseCompareFn;
+}
+
+function composeFilters<T extends PostFilter | null>(
+  filterToBeAdded: PostFilter | undefined,
+  baseFilter: T
+): T {
+  if (!filterToBeAdded) return baseFilter;
+  if (baseFilter === null) return baseFilter;
+  return ((post) => baseFilter(post) && filterToBeAdded(post)) as typeof baseFilter;
+}
+
+const mostRecentSort: PostSortCompareFn = (postA, postB) => (
+  postB.data.pubDate.valueOf() - postA.data.pubDate.valueOf()
+);
+
+export async function getPaginatedPostCollection(
+  ressourceType: "courses" | "tips",
+  paginate: PaginateFunction,
+  lang: string | undefined,
+  filter?: PostFilter,
+  sortCompareFn?: PostSortCompareFn,
+  postsPerPage?: number,
+) {
+  const posts = (await getCollection(
+    ressourceType,
+    composeFilters(filter, filterPerLanguage(lang) as PostFilter)
+  )).sort(composeCompareFns(sortCompareFn, mostRecentSort));
+
+  return paginate(posts, { pageSize: postsPerPage ?? DEFAULT_POSTS_PER_PAGE });
+}
+
+export function getPaginatedPostCollectionWithTag(tag: string) {
+  return (
+    ressourceType: "courses" | "tips",
+    paginate: PaginateFunction,
+    lang: string | undefined,
+    filter?: PostFilter,
+    sortCompareFn?: PostSortCompareFn,
+    postsPerPage?: number,
+  ) => getPaginatedPostCollection(
+      ressourceType, paginate, lang,
+      composeFilters(filter, (post) => !!(post.data.tags) && post.data.tags.includes(tag)),
+      sortCompareFn,
+      postsPerPage
+    )
+}
+
+export function getPaginatedPostCollectionOfAuthor(author_id: ReferenceDataEntry<"contributors">) {
+  return (
+    ressourceType: "courses" | "tips",
+    paginate: PaginateFunction,
+    lang: string | undefined,
+    filter?: PostFilter,
+    sortCompareFn?: PostSortCompareFn,
+    postsPerPage?: number,
+  ) => getPaginatedPostCollection(
+      ressourceType, paginate, lang,
+      composeFilters(filter, (post) => post.data.mainAuthor === author_id),
+      sortCompareFn,
+      postsPerPage
+    )
+}

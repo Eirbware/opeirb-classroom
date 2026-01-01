26c51fc23322cdf11298822b9ced942d9898d7fa
Convolutio <79534557+Convolutio@users.noreply.github.com>
2026-01-02 00:32:04 +0100

    fix: href solvings + useless titles

--- src/routeData.ts
+++ src/routeData.ts
@@ -1,28 +1,42 @@
-import { defineRouteMiddleware } from '@astrojs/starlight/route-data';
-import { cropPagination, getPostSidebarEntry, returnToIndex } from './utils/starlight-sidebar';
-import { parseAstroCollectionPageId } from './utils/filter_language';
-import { defaultLanguage } from './utils/ui';
+import { defineRouteMiddleware } from "@astrojs/starlight/route-data";
+import {
+  cropPagination,
+  getPostSidebarEntry,
+  returnToIndex,
+} from "./utils/starlight-sidebar";
+import { parseAstroCollectionPageId } from "./utils/filter_language";
+import { defaultLanguage } from "./utils/ui";
 
 export const onRequest = defineRouteMiddleware((context) => {
   const { sidebar, id } = context.locals.starlightRoute;
-  const { lang, sectionId, postId }  = parseAstroCollectionPageId(id);
+  const { lang, sectionId, postId } = parseAstroCollectionPageId(id);
   if (sectionId === "courses") {
     const currentFirstDepthSidebarEntry = getPostSidebarEntry(
-      sidebar, sectionId, postId
+      sidebar,
+      sectionId,
+      postId,
     );
-    if (currentFirstDepthSidebarEntry !== null && currentFirstDepthSidebarEntry.type==="group") {
+    if (
+      currentFirstDepthSidebarEntry !== null &&
+      currentFirstDepthSidebarEntry.type === "group"
+    ) {
       // change the sidebar
       const newSidebar = currentFirstDepthSidebarEntry.entries;
       context.locals.starlightRoute.sidebar = newSidebar;
       // change the pagination (remove unliked prev / next pages)
       context.locals.starlightRoute.pagination = cropPagination(
-        context.locals.starlightRoute.pagination, sectionId, lang === defaultLanguage ? undefined : lang, postId
+        context.locals.starlightRoute.pagination,
+        sectionId,
+        lang === defaultLanguage ? undefined : lang,
+        postId,
       );
     }
-  }
-  else if (sectionId === "tips") {
+  } else if (sectionId === "tips") {
     context.locals.starlightRoute.hasSidebar = false;
-    context.locals.starlightRoute.pagination = returnToIndex(sectionId, lang === defaultLanguage ? undefined : lang);
+    context.locals.starlightRoute.pagination =
+      !postId || !isNaN(Number(postId))
+        ? { prev: undefined, next: undefined }
+        : returnToIndex(sectionId, lang === defaultLanguage ? undefined : lang);
   }
   // WARN: Here, we may have an issue
 });
